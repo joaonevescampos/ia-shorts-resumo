d3afcb3a21a15d8aa7db625b4a5f734d7b94bd91
João Campos <joaon.c.jv@gmail.com>
2023-10-22 20:09:23 -0300

    update

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import cors from "cors"
+import express from "express"
+
+import { convert } from "./convert.js"
+import { download } from "./download.js"
+import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
+import 'dotenv/config'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json({mensagem: 'Está funcionando!'})
+})
+
+
+app.get("/summary/:id", async (request, response) => {
+  try {
+    await download(request.params.id)
+    const audioConverted = await convert()
+    const result = await transcribe(audioConverted)
+
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
+})
+
+app.post("/summary", async (request, response) => {
+  try {
+    const result = await summarize(request.body.text)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
+})
+
+app.listen(process.env.PORT, () => console.log("Server is running on port 3333"))
